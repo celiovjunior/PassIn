c010050f809e97de5d1a65c485b6e1389d6d39da
Célio Júnior <celio_cjr@hotmail.com>
2024-04-05 21:08:03 +0930

    Adicionar arquivos de projeto.

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+
+namespace PassIn.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestEventJson request)
+        {
+            var useCase = new RegisterEventsUseCase();
+            
+            useCase.Execute(request);
+
+            return Created();
+        }
+
+    }
+}
---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventsUseCase.cs
@@ -0,0 +1,36 @@
+﻿using PassIn.Communication.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventsUseCase
+    {
+        public void Execute(RequestEventJson request)
+        {
+            Validate(request);
+        }
+
+        private void Validate(RequestEventJson request)
+        {
+            if (request.MaximumAttendees <= 0)
+            {
+                throw new ArgumentException("Number is invalid");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Title))
+            {
+                throw new ArgumentException("The title is invalid");
+            }
+
+
+            if (string.IsNullOrWhiteSpace(request.Details))
+            {
+                throw new ArgumentException("The details are invalid");
+            }
+        }
+    }
+}
